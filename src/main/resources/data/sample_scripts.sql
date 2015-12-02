46cf7ebba1604d2e7111039936a08accd38271ce
Ahmet Kucuk <ahmetkucuk92@gmail.com>
2015-12-02 01:48:23 -0500

    script file is added

---
+++ src/main/resources/data/sample_scripts.sql
@@ -0,0 +1,13 @@
+SELECT event_type, count(*) as event_count_by_type from ge
+WHERE event_starttime < '2012-05-01 01:14:47' AND event_endtime < '2012-06-01 01:14:47' GROUP BY event_type;
+
+
+SELECT event_type, count(*) as event_count_by_type from ge
+WHERE event_starttime > '2012-05-01 01:14:47' AND event_endtime < '2012-06-01 01:14:47' GROUP BY event_type;
+
+
+select count(*) from ge as g1, ge as g2 where ST_intersects(g1.hrc_bbox, g2.hrc_bbox) AND
+g1.event_starttime < '2012-01-10 04:45:00'::timestamp;
+
+select count(*) from ge as g1, ge as g2
+where ST_intersects(g1.hpc_bbox, g2.hpc_bbox) AND g1.event_starttime < '2012-01-10 04:45:00'::timestamp;
