3832cabb98b9b76c44b3980826e4a1bb9ccde42a
Berkay Aydin <berqi89@gmail.com>
2016-06-13 18:51:57 -0400

    Created paging version of multiple table search with limit and offset

---
+++ src/main/resources/query/spatiotemporal_filter_page_all.sql
@@ -0,0 +1,55 @@
+DROP FUNCTION IF EXISTS spatiotemporal_filter_page_all(text[],character varying, character varying,
+				timestamp, timestamp, real, real, real, real, character VARYING, real, real);
+CREATE OR REPLACE FUNCTION spatiotemporal_filter_page_all(tnames text[],
+				    t_pred character varying,
+				    s_pred character varying,	
+				    tstart timestamp,
+				    tend timestamp,
+				    xmin real,
+				    ymin real,
+				    xmax real,
+				    ymax real,
+				    order_by_col character VARYING,
+				    limit_count real,
+				    offset_count real
+				    )
+RETURNS TABLE(kb_archivid text) AS
+$BODY$
+DECLARE 
+    st_query TEXT;
+    tname TEXT;
+    array_text TEXT;
+    spat_conditions TEXT;
+    st_query_tail TEXT;
+    query_page TEXT;
+    temp_valid boolean;
+    spat_valid boolean;
+BEGIN
+--Usage example: -- select spatiotemporal_filter_page('ar_spt', 'GreaterThan', 'Intersects', '2015-12-01 21:36:23', '2015-12-03 01:36:23', 10, 10, 300, 300, 'event_starttime', 100, 0);
+
+--Allowed temporal predicates are following: 
+--'Equals', 'LessThan', 'GreaterThan', 'Contains', 'ContainedBy',
+--'Overlaps', 'Precedes', 'PrecededBy'
+array_text = 'ARRAY['; --ar_spt', 'ch_spt']'
+
+FOREACH tname in ARRAY tnames
+LOOP
+    array_text = array_text || quote_literal(tname) || ',';
+END LOOP;
+array_text = substring(array_text from 0 for char_length(array_text)) || ']::TEXT[]';
+--RAISE NOTICE 'Array text was : %', array_text;
+
+
+
+RAISE NOTICE '%', tnames;
+st_query = 'select kb_archivid from spatiotemporal_filter_all( ' ;
+st_query = st_query || array_text ||', ''' || t_pred ||''', ''' || s_pred || ''', '''|| tstart;
+st_query = st_query ||''', '''|| tend|| ''', ' || xmin|| ', '|| ymin ||', ' || xmax || ', ' ;
+st_query = st_query ||ymax || ', ''' ||order_by_col ||''') ';
+st_query = st_query || ' LIMIT ' || limit_count || ' OFFSET ' || offset_count;
+
+RETURN QUERY EXECUTE st_query;
+	    
+END;
+$BODY$
+LANGUAGE plpgsql;
