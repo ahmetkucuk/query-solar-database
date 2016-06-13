3253ad5e62e5bdcd75f489a55c15b93ac0eb3624
Berkay Aydin <berqi89@gmail.com>
2016-06-13 18:53:01 -0400

    order by column addition, returns an extra column

--- src/main/resources/query/spatiotemporal_filter_all.sql
+++ src/main/resources/query/spatiotemporal_filter_all.sql
@@ -1,5 +1,5 @@
 DROP FUNCTION IF EXISTS spatiotemporal_filter_all(text[],character varying, character varying,
-				timestamp, timestamp, real, real, real, real);
+				timestamp, timestamp, real, real, real, real, text);
 CREATE OR REPLACE FUNCTION spatiotemporal_filter_all(tnames text[], 
 				    t_pred character varying,
 				    s_pred character varying,	
@@ -8,16 +8,17 @@ CREATE OR REPLACE FUNCTION spatiotemporal_filter_all(tnames text[],
 				    xmin real,
 				    ymin real,
 				    xmax real,
-				    ymax real
+				    ymax real,
+				    order_by_col text
 				    )
-RETURNS TABLE(kb_archivid text) AS
+RETURNS TABLE(kb_archivid text, col timestamp without time zone) AS
 $BODY$
 DECLARE 
     tname TEXT;
     st_query TEXT;
 
 BEGIN
---Usage example: -- select spatiotemporal_filter_all(ARRAY['ar_spt', 'ch_spt']::TEXT[], 'GreaterThan', 'Intersects', '2015-12-01 21:36:23', '2015-12-03 01:36:23', 10, 10, 300, 300);
+--Usage example: -- select spatiotemporal_filter_all(ARRAY['ar_spt', 'ch_spt']::TEXT[], 'GreaterThan', 'Intersects', '2015-12-01 21:36:23', '2015-12-03 01:36:23', 10, 10, 300, 300, 'event_starttime);
 
 --Allowed temporal predicates are following: 
 --'Equals', 'LessThan', 'GreaterThan', 'Contains', 'ContainedBy',
@@ -29,7 +30,7 @@ BEGIN
 
 FOREACH tname IN ARRAY tnames
 LOOP 
-   st_query = 'select spatiotemporal_filter(' || tname ||', ' || t_pred ||', ' || s_pred || ', '|| tstart||', '|| tend|| ', ' || xmin|| ', '|| ymin ||', ' || xmax || ', ' ||ymax ||')';
+   st_query = 'select * from spatiotemporal_filter_order( ''' || tname ||''', ''' || t_pred ||''', ''' || s_pred || ''', '''|| tstart||''', '''|| tend|| ''', ' || xmin|| ', '|| ymin ||', ' || xmax || ', ' ||ymax || ', ''' ||order_by_col ||''')';
    RAISE NOTICE '%', tname;
    RAISE NOTICE '%', st_query;
    RETURN QUERY EXECUTE st_query; --returns multiple times from different tables   
