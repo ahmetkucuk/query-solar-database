b68bf2ee691183965c48bd3f652e5436ec66e4ab
Ahmet Küçük <ahmetkucuk@Ahmets-MacBook-Pro.local>
2016-12-17 02:05:49 -0500

    add unit test for retrieve in chunks

---
+++ src/tests/java/TestQuerySolarDB.java
@@ -0,0 +1,34 @@
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.stubbing.OngoingStubbing;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.PowerMockUtils;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import services.SolarDatabaseAPI;
+
+import java.text.ParseException;
+
+import static org.mockito.Mockito.doNothing;
+
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
+
+/**
+ * Created by ahmetkucuk on 12/17/16.
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(QuerySolarDB.class)
+public class TestQuerySolarDB {
+
+    @Test
+    public void testRetrieveInChunks() throws ParseException {
+        //PowerMockito.when(QuerySolarDB.retrieveAll(null, null, 1)).thenReturn();
+        SolarDatabaseAPI mockApi = Mockito.mock(SolarDatabaseAPI.class);
+        Mockito.doNothing().when(mockApi).downloadAndInsertEvents(Mockito.anyString(), Mockito.anyString(), Mockito.anyInt());
+        QuerySolarDB.retrieveInChunks(mockApi, "2000-07-07T02:00:00", "2010-07-13T02:00:00", 1);
+    }
+
+}
