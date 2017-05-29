22718da54d41eee47abf6a41e9a828500582f0c5
Berkay Aydin <berqi89@gmail.com>
2017-05-29 17:41:44 -0400

    spatial filter now returns spatiotemporal attributes

--- src/main/resources/query/spatial/spatial_filter_common_order.sql
+++ src/main/resources/query/spatial/spatial_filter_common_order.sql
@@ -1,8 +1,9 @@
 DROP FUNCTION IF EXISTS spatial_filter_common_order(character varying,character varying,
 				real, real, real, real);
---Returns the kb_archivid's of the solar events with another attribute that can be
--- used as an ordering attribute. The spatial filter for the solar events are generic
--- spatial predicates. The bounding box (envelope) coordinates are given as parameters.
+--Returns the spatiotemporal attributes of solar events with an ordering
+-- attribute. The spatial filter for the solar events are generic
+-- spatial predicates. The bounding box (envelope) coordinates are 
+-- given as parameters.
 
 CREATE OR REPLACE FUNCTION spatial_filter_common_order(tname character varying, 
 				    s_pred character varying,	
@@ -12,21 +13,36 @@ CREATE OR REPLACE FUNCTION spatial_filter_common_order(tname character varying,
 				    ymax real,
 				    order_by_col text
 				    )
-RETURNS TABLE(kb_archivid text, col timestamp without time zone) AS
+RETURNS TABLE(kb_archivid TEXT, 
+		event_starttime TIMESTAMP, 
+		event_endtime TIMESTAMP, 
+		hpc_boundcc TEXT, 
+		hpc_coord TEXT, 
+		event_type TEXT, 
+		event_coordunit TEXT, 
+		hpc_bbox TEXT, 
+		orderby TIMESTAMP) AS
 $BODY$
+DECLARE
+    col_names TEXT = '';
 BEGIN
---Usage example: -- select spatial_filter_common_order('ar', 'Covers', 
---                                                     10, 10, 200, 200, 
---                                                     'event_starttime');
+--Usage example: 
+-- select spatial_filter_common_order('ar', 'Covers', 
+--                                     10, 10, 200, 200, 
+--                                    'event_starttime');
 
 --Allowed spatial predicates are following: 
 --'Intersects', 'Disjoint', 'Within', 'CoveredBy', 'Covers', 'Overlaps'
 --'Contains', 'Disjoint', 'Equals', 'Touches' and some more.
 
 
+
+col_names = 'kb_archivid, event_starttime, event_endtime, ST_AsText(hpc_boundcc), ST_AsText(hpc_coord), event_type, event_coordunit, ST_AsText(hpc_bbox), ' || order_by_col || ' as orderby';
+
+
 --See the generic spatial boolean functions of PostGIS that starts with "ST_"
     RETURN QUERY EXECUTE 
-	'SELECT kb_archivid, '|| order_by_col ||
+	'SELECT ' || col_names ||  
 	' FROM ' || tname || ' ' ||
 	'WHERE ST_'||s_pred||'( hpc_bbox, ST_MakeEnvelope(' 
 		||xmin||', '||ymin||', '||xmax||', '||ymax||',4326) )'; 
