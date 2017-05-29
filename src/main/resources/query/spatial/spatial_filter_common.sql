d08292ae3187fb71c877d33d94efc78d4aa72118
Berkay Aydin <berqi89@gmail.com>
2017-05-29 18:04:35 -0400

    spatial filter common returns extra parameters if needed.

--- src/main/resources/query/spatial/spatial_filter_common.sql
+++ src/main/resources/query/spatial/spatial_filter_common.sql
@@ -1,6 +1,6 @@
 DROP FUNCTION IF EXISTS spatial_filter_common(text[], character varying,
 				real, real, real, real, text);
---Returns the kb_archivid's (and another attribute to be used for ordering) records 
+--Returns the spatiotemporal attributes of solar event records 
 -- from multiple event tables (e.g., ar, ch, ef) using "spatial_filter_common_order"
 -- function. The spatial predicates bbox coordinates are passed as parameters.
 CREATE OR REPLACE FUNCTION spatial_filter_common(tnames text[], 
@@ -11,7 +11,15 @@ CREATE OR REPLACE FUNCTION spatial_filter_common(tnames text[],
 				    ymax real,
 				    order_by_col text
 				    )
-RETURNS TABLE(kb_archivid text, col timestamp without time zone) AS
+RETURNS TABLE(kb_archivid TEXT, 
+		event_starttime TIMESTAMP, 
+		event_endtime TIMESTAMP, 
+		hpc_boundcc TEXT, 
+		hpc_coord TEXT, 
+		event_type TEXT, 
+		event_coordunit TEXT, 
+		hpc_bbox TEXT, 
+		orderby TIMESTAMP ) AS
 $BODY$
 DECLARE 
     tname TEXT;
