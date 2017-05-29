cb79b650f1482b227d5b419cb4eefc0476f084fe
Berkay Aydin <berqi89@gmail.com>
2017-05-29 16:57:21 -0400

    name change for making it compatible with temporal queries

---
+++ src/main/resources/query/spatial/spatial_filter_common_order.sql
@@ -0,0 +1,28 @@
+DROP FUNCTION IF EXISTS spatial_filter_common_order(character varying,character varying,
+				real, real, real, real);
+CREATE OR REPLACE FUNCTION spatial_filter_common_order(tname character varying, 
+				    s_pred character varying,	
+				    xmin real,
+				    ymin real,
+				    xmax real,
+				    ymax real,
+				    order_by_col text
+				    )
+RETURNS TABLE(kb_archivid text, col timestamp without time zone) AS
+$BODY$
+BEGIN
+--Usage example: -- select spatial_filter_common_order('ar_spt', 'Covers', 
+			-- 10, 10, 200, 200, 'event_starttime');
+--Allowed spatial predicates are following: 
+--'Intersects', 'Disjoint', 'Within', 'CoveredBy', 'Covers', 'Overlaps'
+--'Contains', 'Disjoint', 'Equals', 'Touches' and some more.
+--See the functions of PostGIS that starts with "ST_"
+    RETURN QUERY EXECUTE 
+	'SELECT kb_archivid, '|| order_by_col ||
+	' FROM ' ||tname || ' ' ||
+	'WHERE ST_'||s_pred||'( hpc_bbox, ST_MakeEnvelope(' 
+		||xmin||', '||ymin||', '||xmax||', '||ymax||',4326) )'; 
+		-- bounding box limits
+END;
+$BODY$
+LANGUAGE plpgsql;
