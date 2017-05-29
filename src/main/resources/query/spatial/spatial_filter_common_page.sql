5e9123af00b004be0afa1318a6415f74f7f1b492
Berkay Aydin <berqi89@gmail.com>
2017-05-29 18:10:14 -0400

    spatial filter with paging procedure returns ST attributes too, as in the temporal

--- src/main/resources/query/spatial/spatial_filter_common_page.sql
+++ src/main/resources/query/spatial/spatial_filter_common_page.sql
@@ -1,9 +1,9 @@
 DROP FUNCTION IF EXISTS spatial_filter_common_page(text[], character varying,
 				real, real, real, real, character varying, real, real);
---Returns the kb_archivid's of events from multiple tables that are 
--- filtered using a predicate and a spatial windwo (bounding box, given 
--- with a coordinate). The last two parameters are for paging and limiting. 
--- See the usage example
+--Returns the spatiotemporal attributes of solar events from multiple tables 
+-- that are filtered using a predicate and a spatial window (bounding box, 
+-- given with four coordinates [xmin, xmax, ymin, ymax]). 
+-- The last two parameters are for paging and limiting. See the usage example
 CREATE OR REPLACE FUNCTION spatial_filter_common_page(tnames text[],
 				    s_pred character varying,	
 				    xmin real,
@@ -14,7 +14,15 @@ CREATE OR REPLACE FUNCTION spatial_filter_common_page(tnames text[],
 				    limit_count real,
 				    offset_count real
 				    )
-RETURNS TABLE(kb_archivid text) AS
+RETURNS TABLE(kb_archivid TEXT, 
+		event_starttime TIMESTAMP, 
+		event_endtime TIMESTAMP, 
+		hpc_boundcc TEXT, 
+		hpc_coord TEXT, 
+		event_type TEXT, 
+		event_coordunit TEXT, 
+		hpc_bbox TEXT, 
+		orderby TIMESTAMP ) AS
 $BODY$
 DECLARE 
     s_query TEXT;
@@ -44,10 +52,10 @@ array_text = substring(array_text from 0 for char_length(array_text)) || ']::TEX
 
 
 RAISE NOTICE '%', tnames;
-s_query = 'select kb_archivid from spatial_filter_common( ' ;
+s_query = 'select * from spatial_filter_common( ' ;
 s_query = s_query || array_text || ', ' || quote_literal(s_pred) || ', ' || xmin|| ', '|| ymin ||', ' || xmax || ', ' ;
 s_query = s_query || ymax || ', ' || quote_literal(order_by_col) ||') ';
-s_query = s_query || ' ORDER BY col';
+s_query = s_query || ' ORDER BY ' || order_by_col;
 s_query = s_query || ' LIMIT ' || limit_count || ' OFFSET ' || offset_count;
 RAISE NOTICE 'Query: %', s_query;
 RETURN QUERY EXECUTE s_query;
