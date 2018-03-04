6affb0cd5846b5be6466e13d7c1ba2110359578b
ahmetkucuk <ahmetkucuk92@gmail.com>
2018-03-03 19:42:46 -0500

    register test consumer

--- src/main/java/QuerySolarDB.java
+++ src/main/java/QuerySolarDB.java
@@ -51,6 +51,7 @@ public class QuerySolarDB {
                 TaskManager.mockConnection.setJobStatus(task.getJobID(), JobStatus.COMPLETED, null);
             }
         };
+        queue.registerConsumer(consumer);
     }
 
     public static void startWithFixedRate(final SolarDatabaseAPI api) {
