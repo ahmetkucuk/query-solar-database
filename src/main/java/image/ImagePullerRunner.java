a87bd7006372b41f4d87a1acb38e6af91837432c
jookimmy <30672093+jookimmy@users.noreply.github.com>
2018-06-26 14:28:02 -0400

    added thread sleep time variable and finished all documentation/edits

--- src/main/java/image/ImagePullerRunner.java
+++ src/main/java/image/ImagePullerRunner.java
@@ -54,7 +54,7 @@ public class ImagePullerRunner {
     /**
      * Thread sleep time variable in milliseconds.
      */
-    final static long SleepTime = 5000;
+    final static long sleepTime = 5000;
 
     /**
      * Main class to use the Image Puller Module.
@@ -89,7 +89,7 @@ public class ImagePullerRunner {
         while(true) {
             System.out.println("Waiting for tasks");
             //Timer set to 5 second intervals
-            Thread.sleep(SleepTime);
+            Thread.sleep(sleepTime);
         }
     }
 
